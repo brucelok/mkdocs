47ca84b5f5e9410558b28b337c058db1c6e687ad
BRUCE LOK <brucelok@BRUCEs-MacBook-Pro.local>
2020-04-03 23:22:50 +0800

    create mkdockerize.sh

---
+++ mkdockerize.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+printf "\nBuild Docker image\n"
+docker build -t py-mkdocs .
+printf "\run Docker containe"
+docker run -d -p 8000:8000 py-mkdocs:latest
+printf "\wait for service up"
+sleep 5
+printf "\nopen web browser with URL http://127.0.0.1:8000"
+printf "\nnow test if service is up\n"
+curl -I http://127.0.0.1:8000
