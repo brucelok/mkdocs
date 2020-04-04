03e781324246a53340055f1ddcf4718030641f43
BRUCE LOK <brucelok@BRUCEs-MacBook-Pro.local>
2020-04-04 14:34:34 +0800

    update mkdockerize.sh

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-printf "\nBuild Docker image\n"
+printf "\nBuild image from Dockerfile\n"
 docker build -t py-mkdocs .
-printf "\run Docker containe"
+printf "Run Docker container\n"
 docker run -d -p 8000:8000 py-mkdocs:latest
-printf "\wait for service up and running"
+printf "Wait for service up and running\n"
 sleep 5
 
 if which python > /dev/null; then
   python http_test.py
 else
-  echo "No python installed, skip test"
+  echo "No python installed, skip test\n"
 fi
 
 printf "\nopen web browser with URL http://127.0.0.1:8000\n"
