9c06a0562a1cc5e319ccdedd20d1037fd6546fc7
Bruce <lok.bruce@gmail.com>
2020-04-06 05:20:14 +0000

    revert mkdockerize.sh

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -2,21 +2,14 @@
 printf "\nBuild image from Dockerfile\n"
 docker build -t py-mkdocs .
 printf "Run Docker container\n"
-docker run -d -p 8000:8000 py-mkdocs:latest
+docker run --name my-mkdocs -d -p 8000:8000 py-mkdocs:latest
 printf "Wait for service up and running\n"
 sleep 5
 
 if which python > /dev/null; then
   python http_test.py
 else
-  </dev/tcp/127.0.0.1/8000
-  if [ "$?" -ne 0 ]; then
-    echo "127.0.0.1:8000 failed"
-    exit 1
-  else
-    echo "127.0.0.1:8000 succeeded"
-    exit 0
-  fi
+  echo "No python installed, skip test\n"
 fi
 
 printf "\nopen web browser with URL http://127.0.0.1:8000\n"
