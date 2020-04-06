9796a96a87edde605fa26e5d9332904dd164b48c
Bruce <lok.bruce@gmail.com>
2020-04-06 05:00:27 +0000

    remove /bin/bash from shell script

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -9,7 +9,14 @@ sleep 5
 if which python > /dev/null; then
   python http_test.py
 else
-  /bin/bash -c "</dev/tcp/127.0.0.1/8000"; echo $?
+  </dev/tcp/127.0.0.1/8000
+  if [ "$?" -ne 0 ]; then
+    echo "127.0.0.1:8000 failed"
+    exit 1
+  else
+    echo "127.0.0.1:8000 succeeded"
+    exit 0
+  fi
 fi
 
 printf "\nopen web browser with URL http://127.0.0.1:8000\n"
